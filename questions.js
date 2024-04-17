e87a9e6512bbe8e94dc8a18b5c2b9b3db7920a78
harshini0807 <167302903+harshini0807@users.noreply.github.com>
2024-04-17 15:54:16 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,70 @@
+let questions = [
+  {
+    numb: 1,
+    question:
+      "You felt so good or hyper that other people thought you were not your normal self or were so hyper that you got into trouble?🤷‍♂️🤷‍♀️",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+  {
+    numb: 2,
+    question:
+      "You were so irritable that you shouted at people or started fights or arguments?🤬",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+
+  {
+    numb: 3,
+    question: "You felt much more self-confident than usual?😎",
+    answer: "Yes",
+    options: ["Yes", "No"],
+  },
+  {
+    numb: 4,
+    question:
+      "You got much less sleep than usual and found you didn’t really miss it?🥱",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+
+  {
+    numb: 5,
+    question: " You were much more talkative or spoke much faster than usual?🧑",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+  {
+    numb: 6,
+    question:
+      "Thoughts raced through your head or you couldn’t slow your mind down?💆‍♂️",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+  {
+    numb: 7,
+    question:
+      "You were so easily distracted by things around you that you had trouble concentrating or staying on track?😢",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+  {
+    numb: 8,
+    question: "You had much more energy than usual?💪",
+    answer: "Yes",
+    options: ["Yes", "No"],
+  },
+  {
+    numb: 9,
+    question:
+      "You did things that were unusual for you or that other people might have thought were excessive, foolish, or risky?😬",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+  {
+    numb: 10,
+    question: "Spending money got you or your family into trouble?😥",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+];
